d78580f3dff963e6e2b833a1a4f430fb12c5e8f9
waldo1234567 <waldowalerian@gmail.com>
2025-02-21 20:21:12 +0800

    init commits

---
+++ src/main/java/com/example/summarizer/summarizer/SummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.summarizer.summarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummarizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/summarizer/summarizer/SummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.summarizer.summarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
